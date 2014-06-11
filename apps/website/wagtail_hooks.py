cb22c3c9a40f839b7b9e13846901c48e219898f5
Ben Margolis <ben@sixpearls.com>
2014-06-10 19:01:56 -0700

    basic template for adding hallohtml plugin for raw HTML editing

---
+++ apps/website/wagtail_hooks.py
@@ -0,0 +1,19 @@
+from wagtail.wagtailadmin import hooks
+from django.utils.html import format_html, format_html_join
+from django.conf import settings
+
+def editor_js():
+  return format_html(
+    """
+    <script>
+      registerHalloPlugin('hallohtml');
+    </script>
+    """
+  )
+hooks.register('insert_editor_js', editor_js)
+
+def editor_css():
+  return format_html('<link rel="stylesheet" href="' \
+  + settings.STATIC_URL \
+  + 'website/editor.css">')
+hooks.register('insert_editor_css', editor_css)
